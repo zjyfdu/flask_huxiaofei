f4565c078422efa3a076cf408ff2221bb8473e35
zhaijy <zhaijy@zhaijydeMacBook-Pro.local>
2017-09-18 17:54:52 +0800

    for tmp

--- app/__init__.py
+++ app/__init__.py
@@ -36,4 +36,7 @@ def create_app(config_name):
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
+    from .course import course as course_blueprint
+    app.register_blueprint(course_blueprint, url_prefix='/course')
+
     return app
