db2c54159a2e405c3c42d12d929aebcd804a842e
zhaijy <zhaijy@zhaijydeMacBook-Pro.local>
2017-10-02 15:45:43 +0800

    课程上传页面好了！

--- app/course/forms.py
+++ app/course/forms.py
@@ -1,18 +1,25 @@
 # -*- coding: utf-8 -*-
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField, TextAreaField, FileField
-from wtforms.validators import Required, Length, Email, Regexp, EqualTo
+from wtforms.validators import Required, Length, Email, Regexp, EqualTo, NumberRange
 from wtforms import ValidationError
 from ..models import User
 
 class CourseForm(FlaskForm):
     title = StringField('课程名', validators=[Required('内容不能为空')])
-    price = IntegerField('价格', validators=[Required("内容不为空")])
-    mode = StringField('上课方式', validators=[Required("内容不为空")])
+    price = IntegerField('价格', validators=[Required("请填个自然数")])
+    mode = StringField('上课方式', validators=[Required("内容不能为空")])
     abstract = StringField('课程简介', validators=[Required('内容不能为空')])
     introduction = TextAreaField("课程的详细介绍", validators=[Required("内容不能为空")])
     image = FileField('可以给课程加个展示的图片')
     submit = SubmitField('提交')
+
+    def validate_price(self, field):
+        if field.data<0:
+            raise ValidationError(u'不要赔钱啊')
+        elif field.data>20000:
+            raise ValidationError(u'这么凶')
+
 
 # class LoginForm(FlaskForm):
 #     email = StringField('Email', validators=[Required(), Length(1, 64),
